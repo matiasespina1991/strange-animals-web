3c1c16d86bfcfd382d1750dc3a8f4c820b6570f6
Matias Espina <matiasespina1991@gmail.com>
2026-06-27 13:59:59 +0200

    v13

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import {HomePage} from '@/features/home/HomePage';
+import {CustomCursor} from '@/features/home/components/CustomCursor';
 import {ReleaseIdVerifierPage} from '@/features/release-id-verifier/ReleaseIdVerifierPage';
 import {WebampSkinUploaderPage} from '@/features/webamp-skins/WebampSkinUploaderPage';
 import {usePathRoute} from '@/hooks/usePathRoute';
 
 export function App() {
   const route = usePathRoute();
+  let page = <HomePage />;
 
   if (route === '/services/release-id-verifier') {
-    return <ReleaseIdVerifierPage />;
+    page = <ReleaseIdVerifierPage />;
   }
 
   if (route === '/webamp-skin-uploader') {
-    return <WebampSkinUploaderPage />;
+    page = <WebampSkinUploaderPage />;
   }
 
-  return <HomePage />;
+  return (
+    <>
+      <CustomCursor />
+      {page}
+    </>
+  );
 }
